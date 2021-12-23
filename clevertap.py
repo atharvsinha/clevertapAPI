62e49b24a76ca53395f6553bc1cac6bf74279846
atharv2 <atharv@better.club>
2021-12-23 15:42:00 +0530

    Pixemy CT

--- clevertap.py
+++ clevertap.py
@@ -4,58 +4,117 @@ import time
 from flask import Flask, render_template, request
 
 
-def JSONify(data):
+# def JSONify(data):
+#     events = []
+#     user = []
+#     for i in range(len(data)):
+#         temp = {}
+#         for j in range(4):
+#             column = list(data.keys())[j]
+#             if column=='ts':
+#                 temp[column] = round(time.time(), 0)
+                
+#             else:
+#                 try:
+#                     temp[column] = int(data.iloc[i][column])
+#                 except:
+#                     temp[column] = str(data.iloc[i][column])
+#         temp1 = {}
+#         for j in range(5, 20):
+#             column = list(data.keys())[j]
+#             try:
+#                 temp1[column] = int(data.iloc[i][column])
+#             except:
+#                 temp1[column] = str(data.iloc[i][column])
+#         temp['evtData'] = temp1
+#         events.append(temp)
+
+#         temps = {}
+
+#         column = data.keys()[0]
+
+#         try:
+#             temps[column] = int(data.iloc[i][column])
+#         except:
+#             temps[column] = str(data.iloc[i][column])
+#         column = data.keys()[1]
+#         temps[column] = round(time.time(), 0)
+        
+#         column = data.keys()[20]
+#         try:
+#             temps['type'] = int(data.iloc[i][column])
+#         except:
+#             temps['type'] = str(data.iloc[i][column])
+
+#         temp1 = {}
+#         for j in range(22, len(data.keys())):
+#             column = list(data.keys())[j]
+#             try:
+#                 temp1[column] = int(data.iloc[i][column])
+#             except:
+#                 temp1[column] = str(data.iloc[i][column])
+#         temps['profileData'] = temp1
+#         events.append(temp)
+#         user.append(temps)
+
+    # events = {'d': events}
+    # user = {'d': user}
+
+    # headers = {
+    #     'X-CleverTap-Account-Id': '8WR-899-KR6Z',
+    #     'X-CleverTap-Passcode': 'SCY-KUV-GWUL',
+    #     'Content-Type': 'application/json; charset=utf-8',
+    # }
+
+    # usr = f'''{user}'''
+    # response1 = requests.post(
+    #     'https://api.clevertap.com/1/upload', headers=headers, data=usr)
+
+    
+
+    # events = f'''{events}'''
+    # response2 = requests.post(
+    #     'https://api.clevertap.com/1/upload', headers=headers, data=events)
+#     # if response1.json()['status'] == response2.json()['status'] == 'success':
+#     #     return 'both successful'
+#     # elif response1.json()['status']=='success':
+#     #     return 'usr successful'
+#     # elif response2.json()['status']=='success':
+#     #     return 'evt successful'
+#     # else: 
+#     #     return 'both fail'
+#     return events
+
+def JSONify (data):
     events = []
     user = []
+
+    typeIndex = list(data.columns).index('type.1')
+    profileIndex = list(data.columns).index('profileData')
+    temp1 = {}
     for i in range(len(data)):
         temp = {}
-        for j in range(4):
-            column = list(data.keys())[j]
-            if column=='ts':
-                temp[column] = round(time.time(), 0)
-                
-            else:
-                try:
-                    temp[column] = int(data.iloc[i][column])
-                except:
-                    temp[column] = str(data.iloc[i][column])
+        temp['identity'] = data.iloc[i]['identity']
+        temp['ts'] = int(round(time.time(), 0))
+        temp['type'] = 'event'
+        temp['evtName'] = data.iloc[i]['evtName']
         temp1 = {}
-        for j in range(5, 20):
-            column = list(data.keys())[j]
-            try:
-                temp1[column] = int(data.iloc[i][column])
-            except:
-                temp1[column] = str(data.iloc[i][column])
+        for j in range(5, typeIndex):
+            temp1[list(data.columns)[j]] = str(data.iloc[i][list(data.columns)[j]])
         temp['evtData'] = temp1
         events.append(temp)
-
-        temps = {}
-
-        column = data.keys()[0]
-
-        try:
-            temps[column] = int(data.iloc[i][column])
-        except:
-            temps[column] = str(data.iloc[i][column])
-        column = data.keys()[1]
-        temps[column] = round(time.time(), 0)
-        
-        column = data.keys()[20]
-        try:
-            temps['type'] = int(data.iloc[i][column])
-        except:
-            temps['type'] = str(data.iloc[i][column])
-
+        temp = {}
+        temp['identity'] = data.iloc[i]['identity']
+        temp['ts'] = int(round(time.time(), 0))
+        temp['type'] = data.iloc[i]['type.1']
         temp1 = {}
-        for j in range(22, len(data.keys())):
-            column = list(data.keys())[j]
-            try:
-                temp1[column] = int(data.iloc[i][column])
-            except:
-                temp1[column] = str(data.iloc[i][column])
-        temps['profileData'] = temp1
-        events.append(temp)
-        user.append(temps)
+        for j in range(profileIndex+1, len(data.columns)):
+            if list(data.columns)[j] == 'parent name.1':
+                temp1['parent name'] = data.iloc[i][list(data.columns)[j]]
+            else:
+                temp1[list(data.columns)[j]] = data.iloc[i][list(data.columns)[j]]
+        temp['profileData'] = temp1
+        user.append(temp)
 
     events = {'d': events}
     user = {'d': user}
@@ -70,15 +129,13 @@ def JSONify(data):
     response1 = requests.post(
         'https://api.clevertap.com/1/upload', headers=headers, data=usr)
 
-    print(response1.json())
-
-    events = f'''{events}'''
+    evt = f'''{events}'''
     response2 = requests.post(
-        'https://api.clevertap.com/1/upload', headers=headers, data=events)
-    if response1.json()['status'] == response2.json()['status'] == 'success':
-        return 'success'
-    else:
-        return 'fail'
+        'https://api.clevertap.com/1/upload', headers=headers, data=evt)
+
+    
+    return response1.json(), response2.json(), usr, evt
+    
 
 
 app = Flask(__name__, template_folder='templates')
@@ -94,7 +151,7 @@ def upload_file():
     if request.method == 'POST':
         f = request.files['file']
         data = pd.read_csv(f)
-    data.columns = ["Identity",	'ts',	'type',	'evtName',	'evtData',	'category',	'age group',	'course title',	'lesson number','lesson name',	'preferred date',	'parent name',	'email', 	'course url',	'platform',	'transaction date',	'channel',	'zoom link',	'learning material',	'feedback jotform',	'type.1',	'profileData',	'customer type',	'parent name',	'child name','child birthdate']
+    data.columns = ["identity",	'ts',	'type',	'evtName',	'evtData',	'category',	'age group',	'course title',	'lesson number','lesson name',	'preferred date',	'parent name',	'email', 	'course url',	'platform',	'transaction date',	'channel',	'zoom link',	'learning material',	'feedback jotform',	'type.1',	'profileData',	'customer type',	'parent name.1',	'child name','child birthdate']
 
     return f'''{JSONify(data)}'''
 
